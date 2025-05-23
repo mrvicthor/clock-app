023fdffb16ebaea54893a6fe34dbd4bcbfa5b1ff
Victor Eleanya <victoreleanya89@gmail.com>
2025-05-23 12:49:07 +0100

    feat(main.ts): designed for mobile

--- src/main.ts
+++ src/main.ts
@@ -21,55 +21,55 @@ import { handleToggle } from "./toggle.ts";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
  <main class="${
    hour < 17
-     ? "lg:bg-[url('/assets/desktop/bg-image-daytime.jpg')]"
+     ? "lg:bg-[url('/assets/desktop/bg-image-daytime.jpg')] md:bg-[url('/assets/tablet/bg-image-daytime.jpg')] bg-[url('/assets/mobile/bg-image-daytime.jpg')]"
      : "lg:bg-[url('/assets/desktop/bg-image-nighttime.jpg')] md:bg-[url('/assets/tablet/bg-image-nighttime.jpg')] bg-[url('/assets/mobile/bg-image-nighttime.jpg')]"
  } bg-cover h-dvh w-full">
-<section class="container mx-auto pl-16 lg:px-[10.3125rem] pt-14 pb-24 h-full">
-<div class="h-full flex flex-col justify-between">
+<section class="@container flex justify-center pt-14 pb-24 h-full">
+<div class="flex flex-col justify-between h-full px-4 md:px-16 lg:px-0 w-[23.4375rem] sm:w-[48rem] lg:w-[69.375rem] lg:max-w-[69.375rem]">
 <article class="clock-quote md:h-[25rem]">
-<p class="text-white clock-quote">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
-<p class="text-white font-bold">victor</p>
+<p class="text-white custom-shadow clock-quote">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
+<p class="text-white custom-shadow font-bold">victor</p>
 </article>
-<section class="flex flex-col lg:justify-between h-[22.9375rem] lg:h-[25rem] md:border md:border-red-500">
+<section class="flex flex-col lg:justify-between h-[17rem] md:h-[22.9375rem] lg:h-[25rem]">
 <div class="text-white h-full">
 <div class="flex gap-2 lg:gap-4 items-center"><img src="${
   hour < 17 ? sunLogo : moonLogo
 }" alt="${
   hour < 17 ? "sun" : "moon"
-} logo" class="clock-weather-logo"/> <p class="uppercase tracking-[4px]">${greeting}, it's currently</p></div>
-<h1 class="font-bold text-[6.9375rem] lg:text-[12.5rem]">${time} <span class="text-[1.75rem] font-thin">${abbr}</span></h1>
-<p class="uppercase font-bold text-2xl">in ${city}, ${country_code}</p>
+} logo" class="clock-weather-logo"/> <p class="uppercase text-sm tracking-[4px] custom-shadow">${greeting}<span class="hidden md:inline-block">, it's currently</span></p></div>
+<h1 class="font-bold text-[6.25rem] md:text-[6.9375rem] lg:text-[12.5rem] custom-shadow">${time} <span class="text-[1.75rem] font-thin">${abbr}</span></h1>
+<p class="uppercase font-bold text-sm md:text-2xl custom-shadow">in ${city}, ${country_code}</p>
 </div>
 <div class="relative">
-<button id="toggle" type="button" class="absolute cursor-pointer bottom-0 md:right-0 w-[9.125rem] h-14 bg-white rounded-4xl flex justify-between pl-[1.3125rem] items-center pr-[0.5rem] text-[#303030] font-bold tracking-[0.3125rem] uppercase"></button>
+<button id="toggle" type="button" class="absolute cursor-pointer bottom-0 lg:right-0 w-[9.125rem] h-[2.4375rem] md:h-14 bg-white rounded-4xl flex justify-between pl-[1.3125rem] items-center pr-[0.5rem] text-[#303030] font-bold tracking-[0.3125rem] uppercase"></button>
 </div>
 </section>
-<section id="clock-info" class="clock-info w-full h-[25rem] ${
+<section id="clock-info" class="clock-info w-full h-[16rem] md:h-[27.5rem] lg:h-[25rem] ${
   hour < 17
     ? "bg-white/70 backdrop-blur-sm text-[#303030]"
     : "bg-[#000000]/70 backdrop-blur-sm text-white"
 } fixed left-0 right-0 bottom-0 transform translate-y-full transition-transform duration-700 ease-in-out">
-<div class="container mx-auto px-[10.3125rem] grid grid-cols-2 pt-14 pb-24">
-<div class="flex flex-col gap-[2.625rem]">
-<div class="space-y-[9px]">
-<h2 class="text-xs uppercase tracking-[3px]">current timezone</h2>
-<p class="font-bold text-[3.5rem]">${time_zone.name}</p>
+<div class="px-4 lg:px-[10.3125rem] gap-4 grid md:grid-cols-2 md:justify-items-center content-center h-full">
+<div class="flex flex-col gap-4 md:gap-[2.625rem]">
+<div class="flex items-center justify-between md:flex-col md:items-start">
+<h2 class="text-[0.625rem] sm:text-xs uppercase tracking-[2px] md:tracking-[2.6px] lg:tracking-[3px]">current timezone</h2>
+<p class="font-bold md:text-[2.5rem] lg:text-[3.5rem]">${time_zone.name}</p>
 </div>
-<div>
-<h2 class="text-xs uppercase tracking-[3px]">day of the year</h2>
-<p class="font-bold text-[3.5rem]">${presentDay}</p>
+<div class="flex items-center justify-between md:flex-col md:items-start">
+<h2 class="text-[0.625rem] sm:text-xs uppercase tracking-[2.6px] lg:tracking-[3px]">day of the year</h2>
+<p class="font-bold md:text-[2.5rem] lg:text-[3.5rem]">${presentDay}</p>
 </div>
 </div>
-<div class="flex flex-col gap-[2.625rem] border-l-[1px] ${
+<div class="flex flex-col gap-4 md:gap-[2.625rem] lg:border-l-[1px] ${
   hour < 17 ? "border-[#303030]" : "border-white"
-} pl-[5.875rem]">
-<div class="space-y-[9px]">
-<h2 class="text-xs uppercase tracking-[3px]">day of the week</h2>
-<p class="font-bold text-[3.5rem]">${dayOfTheWeek}</p>
+} lg:pl-[5.875rem]">
+<div class="flex items-center justify-between md:flex-col md:items-start">
+<h2 class="text-[0.625rem] md:text-xs uppercase tracking-[1.6px] lg:tracking-[3px]">day of the week</h2>
+<p class="font-bold md:text-[2.5rem] lg:text-[3.5rem]">${dayOfTheWeek}</p>
 </div>
-<div>
-<h2 class="text-xs uppercase tracking-[3px]">week number</h2>
-<p class="font-bold text-[3.5rem]">${weekNumber}</p>
+<div class="flex items-center justify-between md:flex-col md:items-start">
+<h2 class="text-[0.625rem] sm:text-xs uppercase tracking-[2.6px] lg:tracking-[3px]">week number</h2>
+<p class="font-bold md:text-[2.5rem] lg:text-[3.5rem]">${weekNumber}</p>
 </div>
 </div>
 </div>
--- src/toggle.ts
+++ src/toggle.ts
@@ -7,7 +7,7 @@ export function handleToggle(element: HTMLButtonElement) {
   const span = document.createElement("span")!;
   const clockInfo = document.getElementById("clock-info")!;
   span.classList =
-    "bg-[#303030] h-10 w-10 rounded-full flex items-center justify-center";
+    "bg-[#303030] h-8 w-8 md:h-10 md:w-10 rounded-full flex items-center justify-center";
   const setToggle = () => {
     toggle = !toggle;
     span.innerHTML = toggle
@@ -20,6 +20,7 @@ export function handleToggle(element: HTMLButtonElement) {
   element.addEventListener("click", () => {
     quote.classList.toggle("hidden");
     if (toggle) {
+      console.log(toggle);
       clockInfo.classList.remove("translate-y-full");
       clockInfo.classList.add("translate-y-0");
     } else {
