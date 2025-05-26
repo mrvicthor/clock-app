3ce4827c8ccfcff8a41ae0e077c360640b43a482
Victor Eleanya <victoreleanya89@gmail.com>
2025-05-26 10:27:58 +0100

    feat(main.ts): remove console logs

--- src/main.ts
+++ src/main.ts
@@ -82,31 +82,3 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
 // setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 handleToggle(document.querySelector<HTMLButtonElement>("#toggle")!);
-window.addEventListener("vite:preloadError", (event) => {
-  window.location.reload(); // for example, refresh the page
-});
-
-// <div>
-//   // <h1 class="text-3xl text-red-400 font-bold underline">Hello world!</h1>
-//   //{" "}
-//   <a href="https://vite.dev" target="_blank">
-//     // <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     //{" "}
-//   </a>
-//   //{" "}
-//   <a href="https://www.typescriptlang.org/" target="_blank">
-//     //{" "}
-//     <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     //{" "}
-//   </a>
-//   // <h1>Vite + TypeScript</h1>
-//   //{" "}
-//   <div class="card">
-//     // <button id="counter" type="button"></button>
-//     //{" "}
-//   </div>
-//   //{" "}
-//   <p class="read-the-docs">
-//     // Click on the Vite and TypeScript logos to learn more //{" "}
-//   </p>
-// </div>;
